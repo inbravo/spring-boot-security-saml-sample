de89e277dbef559dceab23e3ae284a763a00c088
Amit Dixit <amtdxt@yahoo.com>
2016-06-07 16:59:17 +0530

    firstcut++
    firstcut++

--- src/main/java/com/vdenotaris/spring/boot/security/saml/web/config/MvcConfig.java
+++ src/main/java/com/vdenotaris/spring/boot/security/saml/web/config/MvcConfig.java
@@ -1,23 +1,23 @@
 /*
  * Copyright 2016 Vincenzo De Notaris
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package com.vdenotaris.spring.boot.security.saml.web.config;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -29,18 +29,23 @@ import com.vdenotaris.spring.boot.security.saml.web.core.CurrentUserHandlerMetho
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
-	@Autowired
-	CurrentUserHandlerMethodArgumentResolver currentUserHandlerMethodArgumentResolver;
-	
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("index");
-        registry.addViewController("/error").setViewName("error");
-    }
-    
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
-    	argumentResolvers.add(currentUserHandlerMethodArgumentResolver);
-    }
+  private static final Logger LOG = LoggerFactory.getLogger(MvcConfig.class);
 
+  @Autowired
+  CurrentUserHandlerMethodArgumentResolver currentUserHandlerMethodArgumentResolver;
+
+  @Override
+  public void addViewControllers(final ViewControllerRegistry registry) {
+
+    LOG.info("addViewControllers");
+    registry.addViewController("/").setViewName("index");
+    registry.addViewController("/error").setViewName("error");
+  }
+
+  @Override
+  public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> argumentResolvers) {
+
+    LOG.info("addArgumentResolvers");
+    argumentResolvers.add(currentUserHandlerMethodArgumentResolver);
+  }
 }
--- src/main/java/com/vdenotaris/spring/boot/security/saml/web/core/CurrentUserHandlerMethodArgumentResolver.java
+++ src/main/java/com/vdenotaris/spring/boot/security/saml/web/core/CurrentUserHandlerMethodArgumentResolver.java
@@ -1,23 +1,23 @@
 /*
  * Copyright 2016 Vincenzo De Notaris
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package com.vdenotaris.spring.boot.security.saml.web.core;
 
 import java.security.Principal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.MethodParameter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
@@ -31,22 +31,35 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 import com.vdenotaris.spring.boot.security.saml.web.stereotypes.CurrentUser;
 
 @Component
-public class CurrentUserHandlerMethodArgumentResolver implements
-		HandlerMethodArgumentResolver {
+public class CurrentUserHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
 
-	public boolean supportsParameter(MethodParameter methodParameter) {
-		return methodParameter.getParameterAnnotation(CurrentUser.class) != null
-				&& methodParameter.getParameterType().equals(User.class);
-	}
+  private static final Logger LOG = LoggerFactory.getLogger(CurrentUserHandlerMethodArgumentResolver.class);
 
-	public Object resolveArgument(MethodParameter methodParameter,
-			ModelAndViewContainer mavContainer, NativeWebRequest webRequest,
-			WebDataBinderFactory binderFactory) throws Exception {
-		if (this.supportsParameter(methodParameter)) {
-			Principal principal = (Principal) webRequest.getUserPrincipal();
-			return (User) ((Authentication) principal).getPrincipal();
-		} else {
-			return WebArgumentResolver.UNRESOLVED;
-		}
-	}
+  /**
+   * 
+   */
+  public final boolean supportsParameter(final MethodParameter methodParameter) {
+
+    LOG.info("supportsParameter, methodParameter: " + methodParameter.getParameterName());
+    return methodParameter.getParameterAnnotation(CurrentUser.class) != null && methodParameter.getParameterType().equals(User.class);
+  }
+
+  /**
+   * 
+   */
+  public final Object resolveArgument(final MethodParameter methodParameter, final ModelAndViewContainer mavContainer,
+      final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory) throws Exception {
+
+    LOG.info("resolveArgument, methodParameter: " + methodParameter.getParameterName());
+
+    if (this.supportsParameter(methodParameter)) {
+
+      final Principal principal = (Principal) webRequest.getUserPrincipal();
+      LOG.info("resolveArgument, principal: " + principal.getName());
+
+      return (User) ((Authentication) principal).getPrincipal();
+    } else {
+      return WebArgumentResolver.UNRESOLVED;
+    }
+  }
 }
